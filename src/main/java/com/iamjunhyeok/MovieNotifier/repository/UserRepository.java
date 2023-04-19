dc86af20185ca2b7e11e9f293ea703f45f0ca8e4
iamjunhyeok <jeonjhyeok@gmail.com>
2023-04-20 00:28:12 +0900

    사용자 목록 조회 Caching

--- src/main/java/com/iamjunhyeok/MovieNotifier/repository/UserRepository.java
+++ src/main/java/com/iamjunhyeok/MovieNotifier/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.iamjunhyeok.MovieNotifier.repository;
 
 import com.iamjunhyeok.MovieNotifier.domain.User;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,6 +10,10 @@ import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Cacheable(value = "users")
     @EntityGraph(attributePaths = "genreRatings")
     List<User> findAll();
+
+    @CacheEvict(value = "users")
+    <S extends User> S save(S entity);
 }
