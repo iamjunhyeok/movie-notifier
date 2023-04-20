0307e21f257b5dd267872e47fc2f01c8c291c849
iamjunhyeok <jeonjhyeok@gmail.com>
2023-04-20 17:09:41 +0900

    알림 메시지를 발송하는 메소드 추가

--- src/main/java/com/iamjunhyeok/MovieNotifier/configuration/CommonConfiguration.java
+++ src/main/java/com/iamjunhyeok/MovieNotifier/configuration/CommonConfiguration.java
@@ -1,5 +1,7 @@
 package com.iamjunhyeok.MovieNotifier.configuration;
 
+import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,8 +9,22 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class CommonConfiguration {
 
+    @Value("${sms.api-key}")
+    private String apiKey;
+
+    @Value("${sms.secret-key}")
+    private String secretKey;
+
+    @Value("${sms.domain}")
+    private String domain;
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public DefaultMessageService defaultMessageService() {
+        return new DefaultMessageService(apiKey, secretKey, domain);
     }
 }
--- src/main/java/com/iamjunhyeok/MovieNotifier/service/NotificationService.java
+++ src/main/java/com/iamjunhyeok/MovieNotifier/service/NotificationService.java
@@ -5,6 +5,11 @@ import com.iamjunhyeok.MovieNotifier.domain.Notification;
 import com.iamjunhyeok.MovieNotifier.domain.User;
 import com.iamjunhyeok.MovieNotifier.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
+import net.nurigo.sdk.message.response.SingleMessageSentResponse;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,15 +22,27 @@ public class NotificationService {
 
     private final NotificationRepository notificationRepository;
 
+    private final DefaultMessageService messageService;
+
+    @Value("${sms.sender}")
+    private String sender;
+
     @Transactional
     public void sendNotification(Movie movie, List<User> users) {
         for (User user : users) {
-            Notification notification = new Notification(getMessage(movie), user, movie);
-            notificationRepository.save(notification);
+            Message message = new Message();
+            message.setFrom(sender);
+            message.setTo(user.getPhoneNumber());
+            message.setText(getMessage(movie, user));
+            SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
+            if (response.getStatusCode().equals("2000")) {
+                Notification notification = new Notification(message.getText(), user, movie);
+                notificationRepository.save(notification);
+            }
         }
     }
 
-    public String getMessage(Movie movie) {
+    public String getMessage(Movie movie, User user) {
         return movie.toString();
     }
 
