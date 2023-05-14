64731414fdab9f4f2d5b07821e8ea917f5b8ea45
iamjunhyeok <jeonjhyeok@gmail.com>
2023-05-15 01:14:33 +0900

    테스트 코드 리팩토링

--- src/test/java/com/iamjunhyeok/MovieNotifier/repository/NotificationRepositoryTest.java
+++ src/test/java/com/iamjunhyeok/MovieNotifier/repository/NotificationRepositoryTest.java
@@ -5,6 +5,7 @@ import com.iamjunhyeok.MovieNotifier.constant.NotificationStatus;
 import com.iamjunhyeok.MovieNotifier.domain.Movie;
 import com.iamjunhyeok.MovieNotifier.domain.Notification;
 import com.iamjunhyeok.MovieNotifier.domain.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,55 +27,37 @@ class NotificationRepositoryTest {
     @Autowired
     private NotificationRepository notificationRepository;
 
-    @Test
-    @DisplayName("사용자에게 발송된 알림 목록 조회")
-    void testFindAllByUserId() {
-        // Arrange
-        User user = new User("jeon", "1231231", "01012345678");
+    private User user;
+
+    private Movie movie;
+
+    private Notification notification;
+
+    @BeforeEach
+    void setup() {
+        user = new User("jeon", "1231231", "01012345678");
         testEntityManager.persist(user);
 
-        Movie movie = new Movie(1L, "무서운영화", LocalDate.of(2023, 5, 11), 90, Genre.HORROR, 8.5F);
+        movie = new Movie(1L, "무서운영화", LocalDate.of(2023, 5, 11), 90, Genre.HORROR, 8.5F);
         testEntityManager.persist(movie);
 
-        Notification notification = new Notification(user, movie);
+        notification = new Notification(user, movie);
         testEntityManager.persist(notification);
-
-        // Act
-        List<Notification> result = notificationRepository.findAllByUserId(user.getId());
-
-        // Assert
-        assertEquals(1, result.size());
-        assertEquals(user.getId(), result.get(0).getUser().getId());
     }
 
     @Test
-    @DisplayName("사용자에게 해당 영화의 알림이 발송된적이 있는지")
-    void testExistsByUserAndMovie() {
+    @DisplayName("사용자에게 해당 영화의 알림 발송 여부 확인")
+    void testCheckIfMovieNotificationWasSentToUser() {
         // Arrange
-        User user = new User("jeon", "1231231", "01012345678");
-        testEntityManager.persist(user);
-
-        Movie movie = new Movie(1L, "무서운영화", LocalDate.of(2023, 5, 11), 90, Genre.HORROR, 8.5F);
-        testEntityManager.persist(movie);
-
-        Notification notification = new Notification(user, movie);
-        testEntityManager.persist(notification);
 
         // Act & Assert
         assertTrue(notificationRepository.existsByUserAndMovie(user, movie));
     }
 
     @Test
-    @DisplayName("상태로 알림 목록 조회")
-    void testFindAllByStatus() {
+    @DisplayName("알림 상태로 알림 목록 조회")
+    void testFindNotificationsByStatus() {
         // Arrange
-        User user = new User("jeon", "1231231", "01012345678");
-        testEntityManager.persist(user);
-
-        Movie movie = new Movie(1L, "무서운영화", LocalDate.of(2023, 5, 11), 90, Genre.HORROR, 8.5F);
-        testEntityManager.persist(movie);
-
-        Notification notification = new Notification(user, movie);
         notification.complete();
         testEntityManager.persist(notification);
 
